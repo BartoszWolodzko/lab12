ea2585948b517871992321dbd90a2c5ea96f1100
BartoszWolodzko <74002652+BartoszWolodzko@users.noreply.github.com>
2023-01-15 17:46:01 +0100

    init

--- zad3/src/components/StarRating.js
+++ zad3/src/components/StarRating.js
@@ -2,18 +2,7 @@ import FaStar from 'react-icons/lib/fa/star';
 export default function StarRating({ rating }) {
     return (
         <div className="star-rating">
-        {[...Array(5)].map((star, i) => {
-            const ratingValue = i + 1;
-            return (
-            <label>
-                <input type="radio" name="rating" value={ratingValue} />
-                <FaStar
-                className="star"
-                color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"}
-                />
-            </label>
-            );
-        })}
+
         </div>
     );
 }
